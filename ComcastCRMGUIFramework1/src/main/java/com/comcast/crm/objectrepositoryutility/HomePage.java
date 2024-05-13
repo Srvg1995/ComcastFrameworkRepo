20d2397fe604bbcf66b550b1e18c21f0716db389
gagan <gagan@Gagan>
2024-05-13 16:34:41 +0530

    added product link in home page

--- ComcastCRMGUIFramework1/src/main/java/com/comcast/crm/objectrepositoryutility/HomePage.java
+++ ComcastCRMGUIFramework1/src/main/java/com/comcast/crm/objectrepositoryutility/HomePage.java
@@ -9,59 +9,75 @@ import org.openqa.selenium.support.PageFactory;
 import com.comcast.crm.generic.webdriverutility.WebDriverUtility;
 
 public class HomePage extends WebDriverUtility{
-	
+
 	WebDriver driver;         
 	public HomePage(WebDriver driver) {       
 		this.driver=driver;
-		  PageFactory.initElements(driver,this);
+		PageFactory.initElements(driver,this);
 	}
 
+	@FindBy(linkText="Products")   
+	private WebElement productLink;
+
+
+	@FindBy(linkText="Organizations")   
+	private WebElement orgLink;
+
+	@FindBy(linkText="Contacts")
+	private WebElement contactLink;
+
+	@FindBy(linkText="Campaigns")
+	private WebElement campaignLink;
+
+	@FindBy(linkText="More")
+	private WebElement moreLink;
+
+	@FindBy(xpath="//img[@src='themes/softed/images/user.PNG']")
+	private WebElement adminImg;
+
+	@FindBy(linkText="Sign Out")
+	private WebElement signOutLnk;
+
+	public WebElement getOrgLink() {
+		return orgLink;
+	}
+
+	public WebElement getContactLink() {
+		return contactLink;
+	}
+
+	public WebElement getCampaignLink() {
+		return campaignLink;
+	}
+
+	public WebElement getMoreLink() {
+		return moreLink;
+	}
 	
-@FindBy(linkText="Organizations")   
-private WebElement orgLink;
+	
+	public WebElement getProductLink() {
+		return productLink;
+	}
 
-@FindBy(linkText="Contacts")
-private WebElement contactLink;
+	public WebElement getAdminImg() {
+		return adminImg;
+	}
 
-@FindBy(linkText="Campaigns")
-private WebElement campaignLink;
+	public WebElement getSignOutLnk() {
+		return signOutLnk;
+	}
 
-@FindBy(linkText="More")
-private WebElement moreLink;
+	public void navigateToCampaignPage()
+	{
+		Actions act=new Actions(driver);
+		act.moveToElement(moreLink).perform();
+		campaignLink.click();
+	}
 
-@FindBy(xpath="//img[@src='themes/softed/images/user.PNG']")
-private WebElement adminImg;
-
-@FindBy(linkText="Sign Out")
-private WebElement signOutLnk;
-
-public WebElement getOrgLink() {
-	return orgLink;
-}
-
-public WebElement getContactLink() {
-	return contactLink;
-}
-
-public WebElement getCampaignLink() {
-	return campaignLink;
-}
-
-public WebElement getMoreLink() {
-	return moreLink;
-}
-
-public void navigateToCampaignPage()
-{
-	Actions act=new Actions(driver);
-	act.moveToElement(moreLink).perform();
-	campaignLink.click();
-}
-
-public void logout()
-{
-	mousemoveOnElement(driver, adminImg);
-	signOutLnk.click();
-}
+	public void logout()
+	{
+		mousemoveOnElement(driver, adminImg);
+		signOutLnk.click();
+	}
 
 }
