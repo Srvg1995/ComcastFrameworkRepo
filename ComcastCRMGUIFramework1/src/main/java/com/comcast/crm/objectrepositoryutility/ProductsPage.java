2b380be01c6ed410eae37feca684d8812ba492a7
gagan <gagan@Gagan>
2024-05-13 17:05:46 +0530

    ProductPage is Added in POM class

---
+++ ComcastCRMGUIFramework1/src/main/java/com/comcast/crm/objectrepositoryutility/ProductsPage.java
@@ -0,0 +1,10 @@
+package com.comcast.crm.objectrepositoryutility;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class ProductsPage {
+
+	@FindBy(xpath="//input[@alt='Create Product...']")
+	private WebElement createProductImgBtn;
+}
